de30737cc3bdceab33a9d1156a53a453dd375b68
sunny kumar <sunny.kumar@terralogic.com>
2024-05-17 12:09:15 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Pagination Example'),
+        title: const Text('Flutter  Example'),
       ),
       body: Column(
         children: [
